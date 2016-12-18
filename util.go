2d9e6093c11e16b4b40ef8374bb0c9249ef4af72
linyows <linyows@gmail.com>
2016-12-19 01:34:11 +0900

    add util

---
+++ util.go
@@ -0,0 +1,25 @@
+package main
+
+import "os"
+
+// FileExists checks file exists
+func FileExists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+
+// CreateFile creates file
+func CreateFile(name string) error {
+	fo, err := os.Create(name)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		fo.Close()
+	}()
+	return nil
+}
