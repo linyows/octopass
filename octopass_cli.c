3e3a82dc7e1c758268d2d829755edbdf2f340e98
linyows <linyows@gmail.com>
2017-02-18 00:31:10 +0900

    no getopt

--- octopass_cli.c
+++ octopass_cli.c
@@ -1,4 +1,3 @@
-#include <getopt.h>
 #include "nss_octopass.c"
 
 #define no_argument 0
@@ -19,124 +18,81 @@ extern void call_splist(void);
 
 void help(void)
 {
-  printf("Usage: octopass [options] [ARG]\n");
+  printf("Usage: octopass [options] [CMD]\n");
   printf("\n");
-  printf("SSHD:\n");
-  printf("  -- [USER]        get public keys from github\n");
-  printf("\n");
-  printf("PAM:\n");
-  printf("  --pam [USER]     receive the password from stdin and return the auth result with the exit status)\n");
-  printf("\n");
-  printf("NSS:\n");
-  printf("  --passwd         show like /etc/passwds, call setpwent(3), getpwent(3), endpwent(3)\n");
-  printf("  --pwnam [USER]   get a passwd by name, call getpwnam(3)\n");
-  printf("  --pwuid [UID]    get a passwd by uid, call getgpwuid(3)\n");
-  printf("  --group          get groups, call setgrent(3), getgrent(3), endgrent(3)\n");
-  printf("  --grnam [GROUP]  get a group by name, call getgrnam(3)\n");
-  printf("  --grgid [GID]    get a group by gid, call getgpwuid(3)\n");
-  printf("  --shadow         get shadows, call setspent(3), getspent(3), endspent(3)\n");
-  printf("  --spnam [USER]   get a shadow by name, call getspnam(3)\n");
+  printf("Commands:\n");
+  printf("  [USER]               get public keys from github\n");
+  printf("  pam [user]           receive the password from stdin and return the auth result with the exit status)\n");
+  printf("  getent passwd [key]  displays passwd entries as octopass nss module\n");
+  printf("  getent shadow [key]  displays shadow passwd entries as octopass nss module\n");
+  printf("  getent group [key]   displays group entries as octopass nss module\n");
   printf("\n");
   printf("Options:\n");
-  printf("  -h, --help       show this help message and exit\n");
-  printf("  -v, --version    print the version and exit\n");
+  printf("  -h, --help           show this help message and exit\n");
+  printf("  -v, --version        print the version and exit\n");
   printf("\n");
 }
 
 int main(int argc, char **argv)
 {
-  int option_index         = 0;
-  struct option longopts[] = {
-      {"pam", no_argument, 0, "a"},       {"passwd", no_argument, 0, "p"},  {"pwnam", required_argument, 0, 0},
-      {"pwuid", required_argument, 0, 0}, {"group", no_argument, 0, "g"},   {"grnam", required_argument, 0, 0},
-      {"grgid", required_argument, 0, 0}, {"shadow", no_argument, 0, "s"},  {"spnam", required_argument, 0, 0},
-      {"help", no_argument, 0, "h"},      {"version", no_argument, 0, "v"}, {0, 0, 0, 0},
-  };
-
-  int c = getopt_long(argc, argv, "ap0:1:g2:3:s4:hv", longopts, &option_index);
-  if (c == -1) {
+  if (argc < 1 || !strcmp(argv[1], "--help") || !strcmp(argv[1], "-h")) {
     help();
     return 2;
   }
 
-  if (c == 0) {
-    help();
-    return 2;
+  if (!strcmp(argv[1], "--version") || !strcmp(argv[1], "-v")) {
+    printf("%s\n", OCTOPASS_VERSION_WITH_NAME);
+    return 0;
   }
 
-  switch (c) {
-  case 'a':
-    break;
-
-  case 'p':
-    call_pwlist();
-    break;
-
-  case '0':
+  if (strcmp(argv[1], "passwd") == 0) {
     if (argc < 3) {
-      fprintf(stderr, ANSI_COLOR_RED "Error: %s requires a username" ANSI_COLOR_RESET "\n", argv[1]);
-      return 1;
+      call_pwlist();
+    } else {
+      long id = atol(argv[2]);
+      if (id > 0) {
+        uid_t uid = (uid_t)atol(argv[2]);
+        call_getpwuid_r(uid);
+      } else {
+        call_getpwnam_r((char *)argv[2]);
+      }
     }
-    const char *name = (char *)argv[2];
-    call_getpwnam_r(name);
-    break;
-
-  case '1':
-    if (argc < 3) {
-      fprintf(stderr, ANSI_COLOR_RED "Error: %s requires a user id" ANSI_COLOR_RESET "\n", argv[1]);
-      return 1;
-    }
-    uid_t uid = (uid_t)atol(argv[2]);
-    call_getpwuid_r(uid);
-    break;
-
-  case 'g':
-    call_grlist();
-    break;
-
-  case '2':
-    if (argc < 3) {
-      fprintf(stderr, ANSI_COLOR_RED "Error: %s requires a gruop name" ANSI_COLOR_RESET "\n", argv[1]);
-      return 1;
-    }
-    const char *name = (char *)argv[2];
-    call_getgrnam_r(name);
-    break;
-
-  case '3':
-    if (argc < 3) {
-      fprintf(stderr, ANSI_COLOR_RED "Error: %s requires a group id" ANSI_COLOR_RESET "\n", argv[1]);
-      return 1;
-    }
-    gid_t gid = (gid_t)atol(argv[2]);
-    call_getgrgid_r(gid);
-    break;
-
-  case 's':
-    call_splist();
-    break;
-
-  case '4':
-    if (argc < 3) {
-      fprintf(stderr, ANSI_COLOR_RED "Error: %s requires a shadow name" ANSI_COLOR_RESET "\n", argv[1]);
-      return 1;
-    }
-    const char *name = (char *)argv[2];
-    call_getspnam_r(name);
-    break;
-
-  case 'h':
-    help();
-    break;
-
-  case 'v':
-    printf("%s\n", NSS_OCTOPASS_VERSION_WITH_NAME);
-    break;
-
-  default:
-    fprintf(stderr, ANSI_COLOR_RED "Error: Invalid command: %s" ANSI_COLOR_RESET "\n", argv[1]);
-    return 1;
+    return 0;
   }
 
+  if (strcmp(argv[1], "group") == 0) {
+    if (argc < 3) {
+      call_grlist();
+    } else {
+      long id = atol(argv[2]);
+      if (id > 0) {
+        gid_t gid = (gid_t)atol(argv[2]);
+        call_getgrgid_r(gid);
+      } else {
+        call_getgrnam_r((char *)argv[2]);
+      }
+    }
+    return 0;
+  }
+
+  if (strcmp(argv[1], "shadow") == 0) {
+    if (argc < 3) {
+      call_splist();
+    } else {
+      long id = atol(argv[2]);
+      if (id > 0) {
+        fprintf(stderr, ANSI_COLOR_RED "[Error]" ANSI_COLOR_RESET " Invalid arguments: %s\n", argv[2]);
+      } else {
+        call_getspnam_r((char *)argv[2]);
+      }
+    }
+    return 0;
+  }
+
+  if (strcmp(argv[1], "pam") == 0) {
+    return 0;
+  }
+
+  fprintf(stderr, ANSI_COLOR_RED "[Error]" ANSI_COLOR_RESET " Invalid command: %s\n", argv[1]);
   return 0;
 }
