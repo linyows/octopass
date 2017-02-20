41c688ff1f1ae8018948067f1dc28c634f1f9548
linyows <linyows@gmail.com>
2017-02-20 09:57:51 +0900

    add copy to headers

--- octopass.c
+++ octopass.c
@@ -1,3 +1,19 @@
+/* Management linux user and authentication with the organization/team on Github.
+   Copyright (C) 2017 Tomohisa Oda
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
 #include "octopass.h"
 
 static size_t write_response_callback(void *contents, size_t size, size_t nmemb, void *userp)
