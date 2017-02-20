07d7b63cbdf725cd2c4d38603bf506acee363fbd
linyows <linyows@gmail.com>
2017-02-20 11:49:16 +0900

    add tests

--- octopass_test.c
+++ octopass_test.c
@@ -27,52 +27,17 @@ void setup(void)
   }
 }
 
-Test(octopass, export_file)
+Test(octopass, remove_quotes)
 {
-  char *f = "/tmp/octopass-export_file_test_1.txt";
-  char *d = "LINE1\nLINE2\nLINE3\n";
-  octopass_export_file(f, d);
+  char s[] = "\"foo\"";
+  octopass_remove_quotes(&s[0]);
 
-  FILE *fp = fopen(f, "r");
+  cr_assert_str_eq(s, "foo");
 
-  if (fp == NULL) {
-    cr_assert_fail("File open failure");
-  }
+  char s_contains[] = "\"I'm a \"foo\"\"";
+  octopass_remove_quotes(&s_contains[0]);
 
-  char line[10];
-  int i = 0;
-
-  while (fgets(line, sizeof(line), fp) != NULL) {
-    switch (i) {
-    case 0:
-      cr_assert_str_eq(line, "LINE1\n");
-      break;
-    case 1:
-      cr_assert_str_eq(line, "LINE2\n");
-      break;
-    case 2:
-      cr_assert_str_eq(line, "LINE3\n");
-      break;
-    }
-    i += 1;
-  }
-
-  fclose(fp);
-}
-
-Test(octopass, import_file)
-{
-  char *f1 = "/tmp/octopass-import_file_test_1.txt";
-  char *d1 = "LINE1\nLINE2\nLINE3\n";
-  octopass_export_file(f1, d1);
-  const char *data1 = octopass_import_file(f1);
-  cr_assert_str_eq(data1, d1);
-
-  char *f2 = "/tmp/octopass-import_file_test_2.txt";
-  char *d2 = "LINEa\nLINEb\nLINEc\n";
-  octopass_export_file(f2, d2);
-  const char *data2 = octopass_import_file(f2);
-  cr_assert_str_eq(data2, d2);
+  cr_assert_str_eq(s_contains, "I'm a \"foo\"");
 }
 
 Test(octopass, truncate)
@@ -163,17 +128,105 @@ Test(octopass, config_loading)
   cr_assert(con.syslog == false);
 }
 
-Test(octopass, remove_quotes)
+Test(octopass, export_file)
 {
-  char s[] = "\"foo\"";
-  octopass_remove_quotes(&s[0]);
+  char *f = "/tmp/octopass-export_file_test_1.txt";
+  char *d = "LINE1\nLINE2\nLINE3\n";
+  octopass_export_file(f, d);
 
-  cr_assert_str_eq(s, "foo");
+  FILE *fp = fopen(f, "r");
 
-  char s_contains[] = "\"I'm a \"foo\"\"";
-  octopass_remove_quotes(&s_contains[0]);
+  if (fp == NULL) {
+    cr_assert_fail("File open failure");
+  }
 
-  cr_assert_str_eq(s_contains, "I'm a \"foo\"");
+  char line[10];
+  int i = 0;
+
+  while (fgets(line, sizeof(line), fp) != NULL) {
+    switch (i) {
+    case 0:
+      cr_assert_str_eq(line, "LINE1\n");
+      break;
+    case 1:
+      cr_assert_str_eq(line, "LINE2\n");
+      break;
+    case 2:
+      cr_assert_str_eq(line, "LINE3\n");
+      break;
+    }
+    i += 1;
+  }
+
+  fclose(fp);
+}
+
+Test(octopass, import_file)
+{
+  char *f1 = "/tmp/octopass-import_file_test_1.txt";
+  char *d1 = "LINE1\nLINE2\nLINE3\n";
+  octopass_export_file(f1, d1);
+  const char *data1 = octopass_import_file(f1);
+  cr_assert_str_eq(data1, d1);
+
+  char *f2 = "/tmp/octopass-import_file_test_2.txt";
+  char *d2 = "LINEa\nLINEb\nLINEc\n";
+  octopass_export_file(f2, d2);
+  const char *data2 = octopass_import_file(f2);
+  cr_assert_str_eq(data2, d2);
+}
+
+Test(octopass, github_request_without_cache, .init = setup)
+{
+  struct config con;
+  struct response res;
+  char *f = "octopass.conf.example";
+  octopass_config_loading(&con, f);
+  char *url = "https://api.github.com/";
+  char *token;
+  octopass_github_request_without_cache(&con, url, &res, token);
+
+  cr_assert_eq(200, res.httpstatus);
+  cr_assert_str_eq(
+      "{\"current_user_url\":\"https://api.github.com/user\",\"current_user_authorizations_html_url\":\"https://"
+      "github.com/settings/connections/applications{/client_id}\",\"authorizations_url\":\"https://api.github.com/"
+      "authorizations\",\"code_search_url\":\"https://api.github.com/search/"
+      "code?q={query}{&page,per_page,sort,order}\",\"commit_search_url\":\"https://api.github.com/search/"
+      "commits?q={query}{&page,per_page,sort,order}\",\"emails_url\":\"https://api.github.com/user/"
+      "emails\",\"emojis_url\":\"https://api.github.com/emojis\",\"events_url\":\"https://api.github.com/"
+      "events\",\"feeds_url\":\"https://api.github.com/feeds\",\"followers_url\":\"https://api.github.com/user/"
+      "followers\",\"following_url\":\"https://api.github.com/user/following{/target}\",\"gists_url\":\"https://"
+      "api.github.com/gists{/gist_id}\",\"hub_url\":\"https://api.github.com/hub\",\"issue_search_url\":\"https://"
+      "api.github.com/search/issues?q={query}{&page,per_page,sort,order}\",\"issues_url\":\"https://api.github.com/"
+      "issues\",\"keys_url\":\"https://api.github.com/user/keys\",\"notifications_url\":\"https://api.github.com/"
+      "notifications\",\"organization_repositories_url\":\"https://api.github.com/orgs/{org}/"
+      "repos{?type,page,per_page,sort}\",\"organization_url\":\"https://api.github.com/orgs/"
+      "{org}\",\"public_gists_url\":\"https://api.github.com/gists/public\",\"rate_limit_url\":\"https://"
+      "api.github.com/rate_limit\",\"repository_url\":\"https://api.github.com/repos/{owner}/"
+      "{repo}\",\"repository_search_url\":\"https://api.github.com/search/"
+      "repositories?q={query}{&page,per_page,sort,order}\",\"current_user_repositories_url\":\"https://api.github.com/"
+      "user/repos{?type,page,per_page,sort}\",\"starred_url\":\"https://api.github.com/user/starred{/owner}{/"
+      "repo}\",\"starred_gists_url\":\"https://api.github.com/gists/starred\",\"team_url\":\"https://api.github.com/"
+      "teams\",\"user_url\":\"https://api.github.com/users/{user}\",\"user_organizations_url\":\"https://"
+      "api.github.com/user/orgs\",\"user_repositories_url\":\"https://api.github.com/users/{user}/"
+      "repos{?type,page,per_page,sort}\",\"user_search_url\":\"https://api.github.com/search/"
+      "users?q={query}{&page,per_page,sort,order}\"}",
+      res.data);
+}
+
+Test(octopass, github_request_without_cache__when_use_dummy_token, .init = setup)
+{
+  struct config con;
+  struct response res;
+  char *f = "octopass.conf.example";
+  octopass_config_loading(&con, f);
+  char *url   = "https://api.github.com/";
+  char *token = "dummydummydummydummydummydummydummydummy";
+  octopass_github_request_without_cache(&con, url, &res, token);
+
+  cr_assert_eq(401, res.httpstatus);
+  cr_assert_str_eq("{\"message\":\"Bad credentials\",\"documentation_url\":\"https://developer.github.com/v3\"}",
+                   res.data);
 }
 
 Test(octopass, team_id, .init = setup)
@@ -184,4 +237,64 @@ Test(octopass, team_id, .init = setup)
   int id = octopass_team_id(&con);
 
   cr_assert_eq(id, 2244789);
+}
+
+Test(octopass, authentication_with_token, .init = setup)
+{
+  struct config con;
+  char *f = "octopass.conf.example";
+  octopass_config_loading(&con, f);
+  char *user  = "linyows";
+  char *token = getenv("OCTOPASS_TOKEN");
+  int res     = octopass_autentication_with_token(&con, user, token);
+  cr_assert_eq(res, 0);
+}
+
+Test(octopass, authentication_with_token__when_wrong_user, .init = setup)
+{
+  struct config con;
+  char *f = "octopass.conf.example";
+  octopass_config_loading(&con, f);
+  char *user  = "dummy";
+  char *token = getenv("OCTOPASS_TOKEN");
+  int res     = octopass_autentication_with_token(&con, user, token);
+  cr_assert_eq(res, 1);
+}
+
+Test(octopass, authentication_with_token__when_wrong_token, .init = setup)
+{
+  struct config con;
+  char *f = "octopass.conf.example";
+  octopass_config_loading(&con, f);
+  char *user  = "linyows";
+  char *token = "dummydummydummydummydummydummydummydummy";
+  int res     = octopass_autentication_with_token(&con, user, token);
+  cr_assert_eq(res, 1);
+}
+
+Test(octopass, github_user_keys, .init = setup)
+{
+  struct config con;
+  char *f = "octopass.conf.example";
+  octopass_config_loading(&con, f);
+  char *user       = "linyows";
+  const char *keys = octopass_github_user_keys(&con, user);
+  cr_assert_str_eq("ssh-rsa "
+                   "AAAAB3NzaC1yc2EAAAABIwAAAQEAqUJvs1vKgHRMH1dpxYcBBV687njS2YrJ+"
+                   "oeIKvbAbg6yL4QsJMeElcPOlmfWEYsp8vbRLXQCTvv14XJfKmgp8V9es5P/l8r5Came3X1S/"
+                   "muqRMONUTdygCpfyo+BJGIMVKtH8fSsBCWfJJ1EYEesyzxqc2u44yIiczM2b461tRwW+7cHNrQ6bKEY9sRMV0p/"
+                   "zkOdPwle30qQml+AlS1SvbrMiiJLEW75dSSENr5M+P4ciJHYXhsrgLE95+"
+                   "ThFPqbznZYWixxATWEYMLiK6OrSy5aYss4o9mvEBJozyrVdKyKz11zSK2D4Z/"
+                   "JTh8eP+NxAw5otqBmfNx+HhKRH3MhJQ==\nssh-rsa "
+                   "AAAAB3NzaC1yc2EAAAADAQABAAABAQDpfOPDOHf5ZpFLR2dMhK+B3vSMtAlh/HPOQXsolZYmPQW/"
+                   "xGb0U0+rgXVvBEw193q5c236ENdSrk4R2NE/4ipA/"
+                   "awyCYCJG78Llj2SmqPWbuCtv1K06mXwuh6VM3DP1wPGJmWnzf44Eee4NtTvOzMrORdvGtzQAM044h11N24w07vYwlBvW3P+"
+                   "PdxllbBDJv0ns2A1v40Oerh/xLqAN6UpUADv5prPAnpGnVmuhiNHElX96FmY4y1RxWFNyxnb7/"
+                   "wRwp0NnjfTAmJtB9SWJK9UABLfre2HHlX0gBbhj1+LSW+U5jXD8F9BZF4XRtVY3Ep0PnUrdDqjttrYE0mBfsMh\nssh-rsa "
+                   "AAAAB3NzaC1yc2EAAAADAQABAAABAQCbBkU87QyUEmecsQjCcMTdS6iARCUXzMo2awb4c+irGPUvkXxQUljmLFRXCIw+"
+                   "cEKajiS7VY5NLCZ6WVCbd4yIK+3jdNzrf74isiG8GdU+m64gNGaXtKGFaQEXBp9uWqqZgSw+"
+                   "bVMX2ArOtoh3lP96WJQOoXsOuX0izNS5qf1Z9E01J6IpE3xfudpaL4/"
+                   "vY1RnljM+KUoiIPFqS1Q7kJ+8LpHvV1T9SRiocpLThXOzifzwwoo9I6emwHr+"
+                   "kGwODERYWYvkMEwFyOh8fKAcTdt8huUz8n6k59V9y5hZWDuxP/zhnArUMwWHiiS1C5im8baX8jxSW6RoHuetBxSUn5vR\n",
+                   keys);
 }
