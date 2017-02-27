ba927e4f11d9915b6ae03e630fc807ac4f8b1be7
linyows <linyows@gmail.com>
2017-02-27 12:47:09 +0900

    update

--- test/integration.sh
+++ test/integration.sh
@@ -15,10 +15,9 @@ function fail() {
   ALL_PASSED=1
   echo -e "[${CLR_FAIL}FAIL${CLR_RESET}] octopass::$(echo $1 | sed -e "s/test_//")"
   echo -e "${CLR_INFO}Expected${CLR_RESET}:"
-  echo -e "$2"
+  echo    "$2"
   echo -e "${CLR_WARN}Actual${CLR_RESET}:"
-  echo -e "$3"
-  echo ""
+  echo -n "$3"
 }
 
 function setup() {
@@ -36,7 +35,7 @@ function test_octopass_passwd() {
   actual="$($cmd)"
   expected="linyows:x:74049:2000:managed by octopass:/home/linyows:/bin/bash\n"
 
-  if [ $actual == $expected ]; then
+  if [ "x$actual" == "x$expected" ]; then
     pass "${FUNCNAME[0]}"
   else
     fail "${FUNCNAME[0]}" "$expected" "$actual"
@@ -105,8 +104,10 @@ function test_public_keys() {
 
 function run_test() {
   self=$(cd $(dirname $0) && pwd)/$(basename $0)
-  tests=$(grep "function test_" $self | sed -E "s/function (.*)\(\) \{/\1/g")
-  for t in $(echo $tests); do; $t; done
+  tests="$(grep "^function test_" $self | sed -E "s/function (.*)\(\) \{/\1/g")"
+  for t in $(echo $tests); do
+    $t
+  done
 }
 
 run_test
