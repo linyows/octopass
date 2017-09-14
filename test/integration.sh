537b1c951b29c7d175a7d0e4a95caaac24bf6de4
linyows <linyows@gmail.com>
2017-09-14 18:05:46 +0900

    test

--- test/integration.sh
+++ test/integration.sh
@@ -65,8 +65,7 @@ function test_getent_shadow() {
 }
 
 function test_id() {
-  getent group
-  cat /etc/group
+  id linyows
   actual="$(id linyows)"
   expected="uid=74049(linyows) gid=2000(admin) groups=2000(admin)"
 
